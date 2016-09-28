1b180ffbd4a2493dcef8f46e222f9572d6937e1e
Alexander Varfolomeev <sanyok_98@bk.ru>
2016-09-28 03:06:01 +0300

    Correct pep8 style in strassen.py file

--- PPL/Homework#3/strassen.py
+++ PPL/Homework#3/strassen.py
@@ -1,11 +1,12 @@
 import numpy as np
 
+
 def mul_matrix(first_matrix, second_matrix):
     size = int(len(first_matrix[0]))
 
     if size == 1:
         return first_matrix * second_matrix
-    
+
     a_1_1 = first_matrix[:size // 2, :size // 2]
     a_1_2 = first_matrix[:size // 2, size // 2:]
     a_2_1 = first_matrix[size // 2:, :size // 2]
@@ -37,15 +38,17 @@ def mul_matrix(first_matrix, second_matrix):
 
     return return_matrix
 
+
 def input_matrix(input_size_of_matrix):
     degree = np.ceil(np.log2(input_size_of_matrix))
     size_of_matrix = int(2 ** degree)
 
-    matrix = np.zeros(shape = (size_of_matrix, size_of_matrix))
+    matrix = np.zeros(shape=(size_of_matrix, size_of_matrix))
     for i in range(input_size_of_matrix):
         matrix[i][:input_size_of_matrix] = input().split(' ')
-        
+
     return matrix
+
 
 def print_matrix(matrix, size):
     for line in matrix[:size]:
