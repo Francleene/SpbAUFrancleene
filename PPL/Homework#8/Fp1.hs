c709236d5c95ffd448252f2512c0702cab3de667
Alexander Varfolomeev <sanyok_98@bk.ru>
2016-11-23 20:08:36 +0300

    Fix Homework #8 (Haskell 1)

--- PPL/Homework#8/Fp1.hs
+++ PPL/Homework#8/Fp1.hs
@@ -1,39 +1,47 @@
 head' :: [a] -> a
-head' (head : tail) = head
+head' (x : xs) = x
 
 tail' :: [a] -> [a]
 tail' [] = []
-tail' (head : tail) = tail
+tail' (x : xs) = xs
 
 take' :: Int -> [a] -> [a]
 take' 0 list = []
 take' num [] = []
-take' num (head : tail) = head : take' (num - 1) tail
+take' num (x : xs) = x : take' (num - 1) xs
+
+length' :: [a] -> Int
+length' [] = 0
+length' (x : xs) = 1 + length xs
 
 drop' :: Int -> [a] -> [a]
-drop' num list
-    | len <= num = []
-    | otherwise  = reverse . take' (len - num) $ reverse list
-    where len = length list
+drop' 0 list = list
+drop' num [] = []
+drop' num (x : xs) = drop' (num - 1) xs
 
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' f xs = [elem | elem <- xs, f elem]
+filter' f [] = []
+filter' f (x : xs)
+    | f x       = x : filter' f xs
+    | otherwise = filter' f xs
 
 foldl' :: (a -> b -> a) -> a -> [b] -> a
 foldl' func init_value [] = init_value
-foldl' func init_value (head : tail) = func (foldl' func init_value tail) head
+foldl' func init_value (x : xs) = foldl' func new_value xs
+    where new_value = func init_value x
 
 concat' :: [a] -> [a] -> [a]
-concat' first_list second_list = first_list ++ second_list
+concat' [] second_list = second_list
+concat' (x : xs) second_list = x : concat' xs second_list
 
 quickSort' :: Ord a => [a] -> [a]
 quickSort' list
     | len < 2 = list
-    | otherwise = sorted_less_part ++ eq_part ++ sorted_greater_part
-    where len = length list
+    | otherwise = concat' sorted_less_part (concat' eq_part sorted_greater_part)
+    where len = length' list
           pivot = head' list
-          greater_part = filter' (\x -> pivot < x) list
-          less_part = filter' (\x -> x < pivot) list
-          eq_part = filter' (\x -> x == pivot) list
+          greater_part = filter' (pivot <) list
+          less_part = filter' (< pivot) list
+          eq_part = filter' (== pivot) list
           sorted_greater_part = quickSort' greater_part
           sorted_less_part = quickSort' less_part
