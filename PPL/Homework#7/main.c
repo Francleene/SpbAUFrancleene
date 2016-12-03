d85eba0f1ec333bc6d85715a86fad0a7fa378ccc
Alexander Varfolomeev <sanyok_98@bk.ru>
2016-12-03 21:26:15 +0400

    Update main.c

--- PPL/Homework#7/main.c
+++ PPL/Homework#7/main.c
@@ -1,69 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <time.h>
 
 #include "thread_pool.h"
 
-typedef struct WaitingTaskNode {
-    struct WaitingTaskNode * next;
+typedef struct TreeNode {
+	int need_waiting;
     Task * task;
-} WaitingTaskNode;
+    struct TreeNode *left, *right, *parent;
+} TreeNode;
 
-typedef struct WaitingTaskQueue {
-    pthread_mutex_t mutex;
-    WaitingTaskNode * begin, * end;
-} WaitingTaskQueue;
-
-void init_waiting_queue(WaitingTaskQueue * tq, Task * task) {
-    pthread_mutex_init(&tq->mutex, NULL);
-
-    WaitingTaskNode * last_node = malloc(sizeof(WaitingTaskNode));
-    last_node->next = NULL;
-    last_node->task = task;
-
-    tq->begin = last_node;
-    tq->end = last_node;
-}
-
-void add_waiting_task(WaitingTaskQueue * tq, Task * task) {
-    pthread_mutex_lock(&tq->mutex);
-
-    WaitingTaskNode * new_node = malloc(sizeof(WaitingTaskNode));
-
-    new_node->next = NULL;
-    new_node->task = task;
-
-    tq->begin->next = new_node;
-    tq->begin = new_node;
-
-    pthread_mutex_unlock(&tq->mutex);
-}
-
-Task * pop_waiting_task(WaitingTaskQueue * tq) {
-    pthread_mutex_lock(&tq->mutex);
-
-    if (tq->end == NULL) {
-        pthread_mutex_unlock(&tq->mutex);
-        return NULL;
-    }
-
-    WaitingTaskNode * last_node = tq->end;
-    Task *task = last_node->task;
-    tq->end = tq->end->next;
-    free(last_node);
-
-    pthread_mutex_unlock(&tq->mutex);
-
-    return task;
-}
-
-typedef struct {
-    int * arr;
-    size_t left, right, recur_depth;
+typedef struct Data {
+    int *arr;
+    int left, right, recur_depth;
     ThreadPool * thpool;
-    WaitingTaskQueue * waiting_tasks;
+    TreeNode * root;
 } Data;
+
+TreeNode * make_tree_node(Task * task, TreeNode * parent) {
+    TreeNode * new_node = (TreeNode *)malloc(sizeof(TreeNode));
+	
+	new_node->need_waiting = 1;
+    new_node->task = task;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    new_node->parent = parent;
+
+    return new_node;
+}
+
+void * pack_data(int * arr, int left, int right, int recur_depth, ThreadPool * thpool, TreeNode * root) {
+    Data * data = (Data *)malloc(sizeof(Data));
+
+    data->arr = arr;
+    data->left = left;
+    data->right = right;
+    data->recur_depth = recur_depth;
+    data->thpool = thpool;
+    data->root = root;
+
+    return data;
+}
+
+int compar(const void * first, const void * second) {
+    return *(int *)first - *(int *)second;
+}
 
 void swap(int * first, int * second) {
     int tmp = *first;
@@ -71,125 +50,129 @@ void swap(int * first, int * second) {
     *second = tmp;
 }
 
-int compar(const void * first, const void * second) {
-    return *(int *)first - *(int *)second;
-}
-
-Data * pack_data(int * arr, size_t left, size_t right, size_t recur_depth, ThreadPool * thpool, WaitingTaskQueue * tq) {
-    Data * data = malloc(sizeof(Data));
-
-    data->arr = arr;
-    data->left = left;
-    data->right = right;
-    data->recur_depth = recur_depth;
-    data->thpool = thpool;
-    data->waiting_tasks = tq;
-
-    return data;
-}
-
-void partition(int * arr, size_t left, size_t right, size_t * i, size_t * j, int pivot) {
+void partition(int * arr, int left, int right, int * i, int * j, int pivot) {
     *i = left, *j = right - 1;
-    printf("p: %d %d %d\n", *i, *j, pivot);
-    for (size_t k = left; k < right; k++) {
-        printf("%d ", arr[k]);
-    }
-    printf("\n");
     while (*i <= *j) {
         while (arr[*i] < pivot) (*i)++;
         while (arr[*j] > pivot) (*j)--;
         if (*i <= *j) swap(&arr[(*i)++], &arr[(*j)--]);
     }
-    for (size_t k = left; k < right; k++) {
-        printf("%d ", arr[k]);
-    }
-    printf("\n");
 }
 
-// [left, right)
-void quick_sort_t(void * ptr) {
-    Data * data = (Data *)ptr;
+void quick_sort(void * ptr) {
+	struct Data * data = (struct Data *)ptr;
 
-    printf("%d %d %d\n", data->left, data->right, data->recur_depth);
+	int * arr = data->arr;
+	int left  = data->left,
+		right = data->right;
 
-    if (data->right - data->left < 2) {
-        return;
-    }
+	if (right - left < 2) {
+		free(data);
+		return;
+	}
 
-    if (data->recur_depth == 0) {
-        qsort(data->arr + data->left, data->right - data->left, sizeof(int), &compar);
-        for (size_t i = data->left; i < data->right; i++) {
-            printf("%d ", data->arr[i]);
-        }
-        printf("\n");
-        return;
-    }
+	if (data->recur_depth == 0 || right - left < 100) {
+		qsort(arr + left, (size_t)(right - left), sizeof(int), &compar);
+		free(data);
+		return;
+	}
 
-    int value = data->arr[ data->left + rand() % (data->right - data->left) ];
-    size_t first_begin, second_end;
-    partition(data->arr, data->left, data->right, &second_end, &first_begin, value);
+	int value = arr[left + rand() % (right - left)];
+	int first_begin, second_end;
+	partition(arr, left, right, &second_end, &first_begin, value);
 
-    Task * first_task = malloc(sizeof(Task));
-    Task * second_task = malloc(sizeof(Task));
+	Task * left_task = (Task *)malloc(sizeof(Task));
 
-    add_waiting_task(data->waiting_tasks, first_task);
-    add_waiting_task(data->waiting_tasks, second_task);
+	data->root->left = make_tree_node(left_task, data->root);
+	data->root->right = make_tree_node(data->root->task, data->root);
 
-    size_t middle = data->left + (data->right - data->left) / 2;
-    void * first_ptr = (void *)pack_data(data->arr, data->left, first_begin + 1, data->recur_depth - 1, data->thpool, data->waiting_tasks);
-    void * second_ptr = (void *)pack_data(data->arr, second_end, data->right, data->recur_depth - 1, data->thpool, data->waiting_tasks);
+	void * left_ptr = pack_data(arr, left, first_begin + 1, data->recur_depth - 1, data->thpool, data->root->left);
+	void * right_ptr = pack_data(arr, second_end, right, data->recur_depth - 1, data->thpool, data->root->right);
 
-    init_task(first_task, quick_sort_t, first_ptr);
-    init_task(second_task, quick_sort_t, second_ptr);
+	init_task(left_task, quick_sort, left_ptr);
 
-    thpool_submit(data->thpool, first_task);
-    thpool_submit(data->thpool, second_task);
+	thpool_submit(data->thpool, left_task);
+	free(data);
+	quick_sort(right_ptr);
 }
 
-void start_quick_sort(int * arr, size_t size, size_t threads_num, size_t recur_depth) {
-    ThreadPool * thpool = malloc(sizeof(ThreadPool));
+void free_tree_node(TreeNode * node) {
+    free(node->task);
+	free(node);
+}
+
+void start_quick_sort(int * arr, int size, size_t threads_num, int recur_depth) {
+    ThreadPool * thpool = (ThreadPool *)malloc(sizeof(ThreadPool));
     thpool_init(thpool, threads_num);
 
-    Task * main_task = malloc(sizeof(Task));
+    Task * main_task = (Task *)malloc(sizeof(Task));
+    TreeNode * root = make_tree_node(main_task, NULL);
 
-    WaitingTaskQueue * waiting_tasks = (WaitingTaskQueue *)malloc(sizeof(WaitingTaskQueue));
-    init_waiting_queue(waiting_tasks, main_task);
-
-    void * ptr = pack_data(arr, 0, size, recur_depth, thpool, waiting_tasks);
-
-    init_task(main_task, quick_sort_t, ptr);
+    void * ptr = pack_data(arr, 0, size, recur_depth, thpool, root);
+    init_task(main_task, quick_sort, ptr);
 
     thpool_submit(thpool, main_task);
+    TreeNode * traveller = root;
+    Task * waiting_task;
 
-    thpool_wait(main_task);
+    while (traveller != NULL) {
+		thpool_wait(traveller->task);
+		traveller->need_waiting = 0;
 
-    Task * waiting_task = pop_waiting_task(waiting_tasks);
-    while (waiting_task != NULL) {
-        thpool_wait(waiting_task);
-        free(waiting_task);
-        waiting_task = pop_waiting_task(waiting_tasks);
+        if (traveller->left != NULL) {
+			if (traveller->left->need_waiting) {
+				traveller = traveller->left;
+			} else {
+				free_tree_node(traveller->left);
+				traveller->left = NULL;
+			}
+			continue;
+		}
+
+		if (traveller->right != NULL) {
+			if (traveller->right->need_waiting) {
+				traveller = traveller->right;
+			} else {
+				free(traveller->right);
+				traveller->right = NULL;
+			}
+			continue;
+		}
+		
+		traveller = traveller->parent;
     }
 
-    free(waiting_tasks);
+    free_tree_node(root);
 
     thpool_finit(thpool);
+    free(thpool);
 }
 
-int main() {
-    //srand(time(NULL));
-    int * arr = (int *)malloc(20 * sizeof(int));
-    for (int i = 0; i < 20; i++) {
-        arr[i] = 1 + rand() % 20;
-        printf("%d ", arr[i]);
+void check_sort(int * arr, int size) {
+	for (int i = 1; i < size; i++) {
+		if (arr[i] < arr[i - 1]) {
+			printf("You're a fucking stupid ass! It doesn't work!\n");
+			exit(1);
+		}
+	}
+	printf("A great piece of luck! It works!\n");
+}
+
+int main(int argc, char ** argv) {
+	int num_threads = atoi(argv[1]),
+		size = atoi(argv[2]),
+		rec_depth = atoi(argv[3]);
+
+	srand(42);
+
+    int	* arr = (int *)malloc(size * sizeof(int));
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand();
     }
 
-    printf("\n");
+    start_quick_sort(arr, size, num_threads, rec_depth);
 
-    start_quick_sort(arr, 20, 100, 100);
+	check_sort(arr, size);
 
-    for (int i = 0; i < 20; i++) {
-        printf("%d ", arr[i]);
-    }
-
-    return 0;
+    free(arr);
 }
