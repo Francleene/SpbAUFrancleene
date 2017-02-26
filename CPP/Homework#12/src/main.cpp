daa58400b5927e739c0bad4cbb3c483a2a0e365e
Alexander Varfolomeev <sanyok_98@bk.ru>
2017-02-26 19:05:16 +0300

    Update main.cpp

--- CPP/Homework#12/src/main.cpp
+++ CPP/Homework#12/src/main.cpp
@@ -2,10 +2,18 @@
 #include <fstream>
 #include <vector>
 #include <cstring>
+#include <cassert>
 
 #include "../include/product.hpp"
 #include "../include/my_vector.hpp"
 #include "../include/test_function.hpp"
+
+template<class T>
+void test() {
+    my_vector<T> v;
+    v.resize(5);
+    assert(v[4] == T());
+}
 
 int main() {
     int x1(1), x2(2);
@@ -13,6 +21,8 @@ int main() {
 	
 	Product p1("asdf", 4, 12.0), p2("qwe", -1, 7.5);
 	test_my_vector<Product>(p1, p2);
+    
+    test<Product>();
 
     return 0;
 }
